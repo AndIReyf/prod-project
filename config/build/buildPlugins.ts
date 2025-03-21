75252af05f0a70485220ee25743548b99e0ce1bf
Andrey Prudius <andrey.prudius@welmed.it>
2025-03-21 16:57:43 +0100

    Fixed webpack dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,30 +7,37 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => [
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new HtmlWebpackPlugin({
-    template: paths.html,
-  }),
-  new ProgressPlugin({
-    activeModules: false,
-    entries: true,
-    modules: true,
-    modulesCount: 5000,
-    profile: false,
-    dependencies: true,
-    dependenciesCount: 10000,
-    percentBy: null,
-  }),
-  new DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-    defaultSizes: 'stat',
-  }),
-];
+export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+  const plugins: WebpackPluginInstance[] = [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new ProgressPlugin({
+      activeModules: false,
+      entries: true,
+      modules: true,
+      modulesCount: 5000,
+      profile: false,
+      dependencies: true,
+      dependenciesCount: 10000,
+      percentBy: null,
+    }),
+    new DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      defaultSizes: 'stat',
+    }));
+  }
+
+  return plugins;
+};
